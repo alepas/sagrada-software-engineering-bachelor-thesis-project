e6255526c0389c82328951f951a462da69b594e5
Davide Piovani <davide.piovani1@gmail.com>
2018-04-24 18:01:54 +0200

    Toolcard interface

--- src/main/java/it/polimi/ingsw/model/cards/ToolCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/ToolCard.java
@@ -1,14 +1,12 @@
 package it.polimi.ingsw.model.cards;
 
-import it.polimi.ingsw.model.WPC.WPC;
-import it.polimi.ingsw.model.dicebag.Dice;
-import it.polimi.ingsw.model.dicebag.DiceBag;
-import it.polimi.ingsw.model.game.RoundTrack;
+import it.polimi.ingsw.model.usersdb.PlayerInGame;
 
 import java.util.ArrayList;
 
 public abstract class ToolCard {
     private String id;
+    private Boolean used = false;
 
     //Deve restitituire un ArrayList di Stringhe contententi tutti gli ID delle ToolCards
     //L'ordine non è importante
@@ -16,15 +14,16 @@ public abstract class ToolCard {
         return null;
     }
 
-
     //Passandogli l'id deve restituire l'oggetto toolcard corrispondente all'id passato
     public static ToolCard getCardByID(String id){
         return null;
     }
 
-    public String getID(){
-        return id;
-    }
+    public String getID(){ return id; }
 
-    public abstract void use(WPC wpc, DiceBag diceBag, ArrayList<Dice> extractedDices, RoundTrack roundTrack);
+    public Boolean getUsed() { return used; }
+
+    public abstract void use(PlayerInGame player);
+
+
 }
