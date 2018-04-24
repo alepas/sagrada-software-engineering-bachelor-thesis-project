2cccfbcfd453722fbda4c8fecda1a02b346dc9f2
Unknown <luca.zorzenon@mail.polimi.it>
2018-04-24 11:45:51 +0200

    Auto stash before merge of "master" and "origin/master"
    LoadingFile Test

--- src/test/java/it/polimi/ingsw/model/usersdb/LoadingFromFileTest.java
+++ src/test/java/it/polimi/ingsw/model/usersdb/LoadingFromFileTest.java
@@ -1,12 +1,28 @@
 package it.polimi.ingsw.model.usersdb;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.mock.*;
+
+import java.io.FileNotFoundException;
 
 
 public class LoadingFromFileTest {
+    Object prova;
+
     @Test
+    void LoadWrongFile(){
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+                    LoadingFromFile.fromFile("it/polimi/ingsw/model/userdb/testnottrue.db");
+                }
+        );
+    }
+
+
+
+
 
 
 
