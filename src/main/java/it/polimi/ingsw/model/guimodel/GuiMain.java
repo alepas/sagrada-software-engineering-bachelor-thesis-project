83fbde2084356b3616c2effe2e2649454039ea0c
Alessandra Pasini <alessandra.pasini@mail.polimi.it>
2018-05-10 00:30:07 +0200

    aggiunta scena selezione wpc

--- src/main/java/it/polimi/ingsw/model/guimodel/GuiMain.java
+++ src/main/java/it/polimi/ingsw/model/guimodel/GuiMain.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.guimodel;
 
+import it.polimi.ingsw.control.guicontroller.Controller;
 import it.polimi.ingsw.control.network.rmi.RmiClient;
 import it.polimi.ingsw.control.network.socket.SocketClient;
 import it.polimi.ingsw.model.constants.NetworkConstants;
@@ -68,6 +69,7 @@ public class GuiMain extends Application {
                     NetworkConstants.SERVER_ADDRESS, NetworkConstants.SOCKET_SERVER_PORT);
 
             client.init();
+
             Application.launch(GuiMain.class, null);
 
 
