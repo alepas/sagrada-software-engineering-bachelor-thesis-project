3cb6ce18ba7e217cded62850c0141a5b4221a929
Luca Zorzenon <luca.zorzenon@mail.polimi.it>
2018-06-22 02:52:14 +0200

    notification typo fixed

--- src/main/java/it/polimi/ingsw/control/network/commands/notifications/ToolCardDiceChangedNotification.java
+++ src/main/java/it/polimi/ingsw/control/network/commands/notifications/ToolCardDiceChangedNotification.java
@@ -2,25 +2,21 @@ package it.polimi.ingsw.control.network.commands.notifications;
 
 import it.polimi.ingsw.model.clientModel.ClientDice;
 import it.polimi.ingsw.model.clientModel.ClientDiceLocations;
-import it.polimi.ingsw.model.clientModel.ClientRoundTrack;
-import it.polimi.ingsw.model.clientModel.ClientWpc;
-
-import java.util.ArrayList;
 
 public class ToolCardDiceChangedNotification implements Notification {
     public final String username;
     public final ClientDice oldDice;
     public final ClientDice newDice;
     public final ClientDiceLocations oldPosition;
-    public final ClientDiceLocations newPosiition;
+    public final ClientDiceLocations newPosition;
 
 
-    public ToolCardDiceChangedNotification(String username, ClientDice oldDice, ClientDice newDice, ClientDiceLocations oldPosition, ClientDiceLocations newPosiition) {
+    public ToolCardDiceChangedNotification(String username, ClientDice oldDice, ClientDice newDice, ClientDiceLocations oldPosition, ClientDiceLocations newPosition) {
         this.username = username;
         this.oldDice = oldDice;
         this.newDice = newDice;
         this.oldPosition = oldPosition;
-        this.newPosiition = newPosiition;
+        this.newPosition = newPosition;
     }
 
     @Override
