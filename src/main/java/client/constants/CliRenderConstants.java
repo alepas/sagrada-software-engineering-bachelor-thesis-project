928cc8e1d14d674953d07b6325fdcfdc824d4497
Davide Piovani <davide.piovani1@gmail.com>
2018-07-05 23:05:50 +0200

    Fixed cli color problems?

--- src/main/java/client/constants/CliRenderConstants.java
+++ src/main/java/client/constants/CliRenderConstants.java
@@ -18,21 +18,21 @@ public class CliRenderConstants {
 
     //Colors
     // Reset
-    public static final String RESET = "\033" + jsonLanguage.get("RESET");
+    public static final String RESET = "\u001b" + jsonLanguage.get("RESET");
 
     // Regular Colors
-    public static final String BLACK = "\033" + jsonLanguage.get("BLACK");
-    public static final String BLACK_BRIGHT = "\033" + jsonLanguage.get("BLACK_BRIGHT");
-    public static final String RED = "\033" + jsonLanguage.get("RED");
-    public static final String GREEN = "\033" + jsonLanguage.get("GREEN");
-    public static final String YELLOW = "\033" + jsonLanguage.get("YELLOW");
-    public static final String BLUE = "\033" + jsonLanguage.get("BLUE");
-    public static final String PURPLE = "\033" + jsonLanguage.get("PURPLE");
+    public static final String BLACK = "\u001b" + jsonLanguage.get("BLACK");
+    public static final String BLACK_BRIGHT = "\u001b" + jsonLanguage.get("BLACK_BRIGHT");
+    public static final String RED = "\u001b" + jsonLanguage.get("RED");
+    public static final String GREEN = "\u001b" + jsonLanguage.get("GREEN");
+    public static final String YELLOW = "\u001b" + jsonLanguage.get("YELLOW");
+    public static final String BLUE = "\u001b" + jsonLanguage.get("BLUE");
+    public static final String PURPLE = "\u001b" + jsonLanguage.get("PURPLE");
 
     // Background
-    public static final String RED_BACKGROUND = "\033" + jsonLanguage.get("RED_BACKGROUND");
-    public static final String GREEN_BACKGROUND = "\033" + jsonLanguage.get("GREEN_BACKGROUND");
-    public static final String YELLOW_BACKGROUND = "\033" + jsonLanguage.get("YELLOW_BACKGROUND");
-    public static final String BLUE_BACKGROUND = "\033" + jsonLanguage.get("BLUE_BACKGROUND");
-    public static final String PURPLE_BACKGROUND = "\033" + jsonLanguage.get("PURPLE_BACKGROUND");
+    public static final String RED_BACKGROUND = "\u001b" + jsonLanguage.get("RED_BACKGROUND");
+    public static final String GREEN_BACKGROUND = "\u001b" + jsonLanguage.get("GREEN_BACKGROUND");
+    public static final String YELLOW_BACKGROUND = "\u001b" + jsonLanguage.get("YELLOW_BACKGROUND");
+    public static final String BLUE_BACKGROUND = "\u001b" + jsonLanguage.get("BLUE_BACKGROUND");
+    public static final String PURPLE_BACKGROUND = "\u001b" + jsonLanguage.get("PURPLE_BACKGROUND");
 }
